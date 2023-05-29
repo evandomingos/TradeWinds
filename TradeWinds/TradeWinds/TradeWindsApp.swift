8af4cb7afd0cb6fe3470b25eeed42ab3c0f95180
evandomingos <edomingos2019@gmail.com>
2023-05-29 09:49:37 -0400

    Create TradeWindsApp.swift

---
+++ TradeWinds/TradeWinds/TradeWindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeWindsApp.swift
+//  TradeWinds
+//
+//  Created by Evan Domingos on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TradeWindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
